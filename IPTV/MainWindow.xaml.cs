e5fc0b2f58f0185a91d7cce93273d21e614e051c
kib <edeklerk@gmail.com>
2017-06-28 22:47:31 +0200

    Window with play control, simple menu, some actions

--- IPTV/MainWindow.xaml.cs
+++ IPTV/MainWindow.xaml.cs
@@ -23,6 +23,50 @@ namespace IPTV
         public MainWindow()
         {
             InitializeComponent();
+            vlcPlayer.LoadMedia(@"D:\VS_Projects\test.avi");
+            // play video
+            vlcPlayer.Play();
         }
+
+        private void cmFullScreen_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Maximized;
+            WindowStyle = WindowStyle.None;
+        }
+
+        private void cmItem1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void vlcPlayer_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+            {
+                WindowState = WindowState.Normal;
+                WindowStyle = WindowStyle.SingleBorderWindow;
+            }
+            else if (WindowState == WindowState.Normal)
+            {
+                WindowState = WindowState.Maximized;
+                WindowStyle = WindowStyle.None;
+            }
+        }
+
+        private void cmItem3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cmItem2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cmExit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
     }
 }
--- IPTV/Properties/AssemblyInfo.cs
+++ IPTV/Properties/AssemblyInfo.cs
@@ -3,6 +3,7 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
+using Meta.Vlc;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -15,6 +16,7 @@ using System.Windows;
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: VlcSettings(@"..\..\libvlc", "-I", "--dummy-quiet", "--ignore-config", "--no-video-title", "--no-sub-autodetect-file")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
